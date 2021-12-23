370ae23f0ba3485f8444faa43ae746646e188424
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2021-12-23 18:35:46 +0800

    fixup

--- executor/machine_learning.go
+++ executor/machine_learning.go
@@ -3,29 +3,41 @@ package executor
 import (
 	"context"
 	"errors"
-	plannercore "github.com/pingcap/tidb/planner/core"
+	"fmt"
 	"strings"
+
+	plannercore "github.com/pingcap/tidb/planner/core"
+	"github.com/pingcap/tidb/util/chunk"
+	"github.com/pingcap/tidb/util/sqlexec"
 )
 
 type MLCreateModelExecutor struct {
 	baseExecutor
 
-	v *plannercore.MLCreateModel
+	v         *plannercore.MLCreateModel
+	modelType string
 }
 
 func (ml *MLCreateModelExecutor) Open(ctx context.Context) error {
-	parameters := ml.v.Parameters
-	pm := make(map[string]string)
-	for i := 0; i < len(parameters); i += 2 {
-		pm[strings.ToLower(parameters[i])] = pm[parameters[i+1]]
+	hasType := false
+	for i := 0; i < len(ml.v.Parameters); i += 2 {
+		if strings.ToLower(ml.v.Parameters[i]) == "type" {
+			hasType = true
+			ml.modelType = ml.v.Parameters[i+1]
+		}
 	}
-	if _, ok := pm["type"]; !ok {
+	if !hasType {
 		return errors.New("no type parameter")
 	}
-
-
-
+	// TODO: check whether other parameters are valid
 	return nil
+}
+
+func (ml *MLCreateModelExecutor) Next(ctx context.Context, req *chunk.Chunk) error {
+	sql := fmt.Sprintf("insert into mysql.ml_models values ('%v', '%v', '%v', NULL)", ml.v.Model, ml.modelType, "test")
+	exec := ml.ctx.(sqlexec.SQLExecutor)
+	_, err := exec.Execute(ctx, sql)
+	return err
 }
 
 type MLTrainModelExecutor struct {
