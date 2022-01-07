544260dd56111b801aaa488b81f5a30d073809e6
hailanwhu <2013301500074@whu.edu.cn>
2022-01-07 17:36:58 +1100

    update grad

--- executor/machine_learning.go
+++ executor/machine_learning.go
@@ -1,10 +1,15 @@
 package executor
 
 import (
+	"bytes"
 	"context"
+	"encoding/gob"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/pingcap/tidb/util/logutil"
+	"gorgonia.org/gorgonia"
+	"strconv"
 	"strings"
 
 	"github.com/pingcap/tidb/distsql"
@@ -85,10 +90,133 @@ func (ml *MLTrainModelExecutor) train(ctx context.Context, modelType, parameters
 		return nil, err
 	}
 
-	// TODO: init the model accroding to parameters: yifan, lanhai
+	// Init the model according to parameters: yifan, lanhai
+	var params map[string]string
+	if err := json.Unmarshal([]byte(parameters), &params); err != nil {
+		return nil, errors.New("encounter error when decoding parameters")
+	}
+	strNumFeatures, ok := params["n_features"]
+	if !ok {
+		return nil, errors.New("n_features it not specified")
+	}
+	numFeatures, err := strconv.Atoi(strNumFeatures)
+	if err != nil {
+		return nil, errors.New("n_features must be an integer")
+	}
+	strNumClasses, ok := params["n_classes"]
+	if !ok {
+		return nil, errors.New("n_classes is not specified")
+	}
+	numClasses, err := strconv.Atoi(strNumClasses)
+	if err != nil {
+		return nil, errors.New("n_classes must be an integer")
+	}
+	strHiddenUnits, ok := params["hidden_units"]
+	if !ok {
+		return nil, errors.New("hidden_units is not specified")
+	}
+	hiddenUnits, err := string2IntSlice(strHiddenUnits)
+	if err != nil {
+		return nil, errors.New("hidden_units must be an array of integers like [2,3,5]")
+	}
+	strBatchSize, ok := params["batch_size"]
+	if !ok {
+		return nil, errors.New("batch_size is not specified")
+	}
+	batchSize, err := strconv.Atoi(strBatchSize)
+	if err != nil {
+		return nil, errors.New("batch_size must be an integer")
+	}
+	strLearningRate, ok := params["learning_rate"]
+	if !ok {
+		return nil, errors.New("learning_rate is not specified")
+	}
+	learningRate, err := strconv.ParseFloat(strLearningRate, 64)
+	if err != nil {
+		return nil, errors.New("learning_size must be a float")
+	}
+	// TODO: loss function and optimizer/solver can also be added in params
+	logutil.BgLogger().Info(fmt.Sprintf("numFeatures = %v, numClasses = %v, hiddenUnits = %v, batchSize = %v, learningRate = %v", numFeatures, numClasses, hiddenUnits, batchSize, learningRate))
+
+	// construct the computation graph
+	g := gorgonia.NewGraph()
+	x := gorgonia.NewMatrix(g, gorgonia.Float64, gorgonia.WithShape(batchSize, numFeatures), gorgonia.WithName("x"))
+	y := gorgonia.NewMatrix(g, gorgonia.Float64, gorgonia.WithShape(batchSize, numClasses), gorgonia.WithName("y"))
+	learnables := make([]*gorgonia.Node, 0, len(hiddenUnits)+1)
+	weightNum := 0
+	current := x
+	currentLen := x.Shape()[1]
+	for _, hiddenLen := range hiddenUnits {
+		w := gorgonia.NewMatrix(g, gorgonia.Float64, gorgonia.WithShape(currentLen, hiddenLen), gorgonia.WithName(fmt.Sprintf("w%v", weightNum)), gorgonia.WithInit(gorgonia.Gaussian(0, 0.1)))
+		weightNum++
+		learnables = append(learnables, w)
+		currentLen = hiddenLen
+		current, err = gorgonia.Mul(current, w)
+		if err != nil {
+			return nil, err
+		}
+		current, err = gorgonia.Rectify(current)
+		if err != nil {
+			return nil, err
+		}
+		current, err = gorgonia.Dropout(current, 0.5)
+		if err != nil {
+			return nil, err
+		}
+	}
+	w := gorgonia.NewVector(g, gorgonia.Float64, gorgonia.WithShape(currentLen, numClasses), gorgonia.WithName(fmt.Sprintf("w%v", weightNum)), gorgonia.WithInit(gorgonia.Gaussian(0, 0.1)))
+	weightNum++
+	learnables = append(learnables, w)
+	current, err = gorgonia.Mul(current, w)
+	if err != nil {
+		return nil, err
+	}
+	current, err = gorgonia.SoftMax(current)
+	if err != nil {
+		return nil, err
+	}
+
+	// cross entropy loss
+	current, err = gorgonia.Log(current)
+	if err != nil {
+		return nil, err
+	}
+	current, err = gorgonia.Neg(current)
+	if err != nil {
+		return nil, err
+	}
+	current, err = gorgonia.HadamardProd(current, y)
+	if err != nil {
+		return nil, err
+	}
+	loss, err := gorgonia.Mean(current)
+	if err != nil {
+		return nil, err
+	}
+	_, err = gorgonia.Grad(loss, learnables...)
+	if err != nil {
+		return nil, err
+	}
+
+	// compile graph and construct machine
+	_, _, err = gorgonia.Compile(g)
+	if err != nil {
+		return nil, err
+	}
+	solver := gorgonia.NewVanillaSolver(gorgonia.WithLearnRate(learningRate))
 
 	// TODO: init model data: yifan, lanhai
 	var modelData []byte
+	weights := make([]gorgonia.Value, 0, len(hiddenUnits)+1)
+	for _, node := range learnables {
+		weights = append(weights, node.Value())
+	}
+	var encodeBuf bytes.Buffer
+	enc := gob.NewEncoder(&encodeBuf)
+	if err := enc.Encode(weights); err != nil {
+		return nil, err
+	}
+	modelData = encodeBuf.Bytes()
 
 	for iter := 0; iter < 10000; iter++ {
 		req, err := ml.constructMLReq(iter, dataPartitionMap, modelType, parameters, modelData)
@@ -106,9 +234,30 @@ func (ml *MLTrainModelExecutor) train(ctx context.Context, modelType, parameters
 			if data == nil { // no more data
 				break
 			}
+			decodeDuf := bytes.NewBuffer(data.GetData())
+			decoder := gob.NewDecoder(decodeDuf)
+			var grads []gorgonia.Value
+			if err = decoder.Decode(&grads); err != nil {
+				return nil, err
+			}
+			for i, weight := range grads {
+				learnables[i].SetGrad(weight)
+			}
+			vg := gorgonia.NodesToValueGrads(learnables)
+			solver.Step(vg)
 		}
 
 		// TODO: update the model data: yifan, lanhai
+		weights = weights[:0]
+		for _, node := range learnables {
+			weights = append(weights, node.Value())
+		}
+		encodeBuf.Reset()
+		enc := gob.NewEncoder(&encodeBuf)
+		if err := enc.Encode(weights); err != nil {
+			return nil, err
+		}
+		modelData = encodeBuf.Bytes()
 	}
 
 	return modelData, nil
